08850940b8dcc2c32a6b68c3434e7a11d3851143
Yas1th005 <yashpanatala@gmail.com>
2025-07-27 23:47:28 +0530

    Removed All

--- app/projects/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import PageContainer from '../components/layouts/page-container';
-
-export default function layout({ children }: { children: React.ReactNode }) {
-  return <PageContainer>{children}</PageContainer>;
-}
