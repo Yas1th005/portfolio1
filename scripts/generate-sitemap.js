c6c2f8a27cc369887728243ab3e842d29ae92131
Yas1th005 <yashpanatala@gmail.com>
2025-07-28 02:08:28 +0530

    V3

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -19,46 +19,7 @@ const siteMetadata = require('../data/siteMetadata')
     '!pages/api',
   ])
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                // Exclude drafts from the sitemap
-                if (page.search('.md') >= 1 && fs.existsSync(page)) {
-                  const source = fs.readFileSync(page, 'utf8')
-                  const fm = matter(source)
-                  if (fm.data.draft) {
-                    return
-                  }
-                  if (fm.data.canonicalUrl) {
-                    return
-                  }
-                }
-                const path = page
-                  .replace('pages/', '/')
-                  // .replace('data/blog', '/blog')
-                  // .replace('data/snippets', '/snippets')
-                  .replace('public/', '/')
-                  .replace('.js', '')
-                  .replace('.tsx', '')
-                  .replace('.mdx', '')
-                  .replace('.md', '')
-                  .replace('/feed.xml', '')
-                const route = path === '/index' ? '' : path
-
-                if (page.search('pages/404.') > -1 || page.search(`pages/blog/[...slug].`) > -1) {
-                  return
-                }
-                return `
-                        <url>
-                            <loc>${siteMetadata.siteUrl}${route}</loc>
-                        </url>
-                    `
-              })
-              .join('')}
-        </urlset>
-    `
+  const sitemap = ``
 
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
